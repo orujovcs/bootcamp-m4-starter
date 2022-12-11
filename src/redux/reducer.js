a9d2638ce57631234c0e6338740d2a203a28d14c
orujovcs <orujovcs@gmail.com>
2022-12-11 15:27:58 +0400

    added addfavoritefilm

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,16 +1,25 @@
 const intialState = {
     movies: [],
-    favourites: [],
+    favoriteList: [],
     title: "",
     listID: "",
     listMovies: [],
-    movieDetails: []
+    movieDetails: [],
 };
 
 function reducer(state = intialState,action){
     switch(action.type){
         case "SEARCH_MOVIE":
-            return{};
+            return{...state,movies:action.payload.movies,};
+        
+        case "ADD_FAVORITE_FILM":
+            const newState = {...state};
+            const id = action.payload.id;
+            const match = newState.movies.find((item) => item.imdbID === id);
+            if(match){
+                newState.favoriteList = [...newState.favoriteList, {...match}];
+            }
+            return newState;
     }
 }
 
