ef9c3f8c3556d306142e7d1a49a50634f6301a10
orujovcs <orujovcs@gmail.com>
2022-12-11 15:20:32 +0400

    modified Search component

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -6,23 +6,13 @@ export function searchMoveis(movies) {
         },
     };
 }
-function getApi(){
-    const apiKey = "8d1d9e0f";
-    fetch(`http://www.omdbapi.com/?s=shrek&apikey=${apiKey}`)
-    .then((res) => res.json())
-    .then((data) => {
-        dispatch(searchMoveis(data.Search));
-    });
-
+export function fetchMovies(name) {
+    return function(dispatch){
+        const apiKey = "fdd591c8";
+        fetch(`http://www.omdbapi.com/?s=${name}&apikey=${apiKey}`)
+        .then((res) => res.json())
+        .then((data) => {
+            dispatch(searchMoveis(data.Search));
+        });
+    }
 }
-console.log(getApi);
-// export function fetchMovies(name) {
-//     return function(dispatch){
-//         const apiKey = "fdd591c8";
-//         fetch(`http://www.omdbapi.com/?s=${name}&apikey=${apiKey}`)
-//         .then((res) => res.json())
-//         .then((data) => {
-//             dispatch(searchMoveis(data.Search));
-//         });
-//     }
-// }
