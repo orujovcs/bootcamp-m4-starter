c89e54b4c4cec52b0057f9cc22ba1b5a0cc5336b
orujovcs <orujovcs@gmail.com>
2022-12-12 13:02:29 +0400

    added others reducers

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,109 +1,109 @@
 export function searchMoveis(movies) {
     return {
-      type: "SEARCH_MOVIE",
-      payload: {
+        type: "SEARCH_MOVIE",
+        payload: {
         movies: movies,
-      },
-    };
-  }
-  
-  export function fetchMovies(name) {
-    return function (dispatch) {
-      const api = "8d1d9e0f";
-      fetch(`http://www.omdbapi.com/?s=${name}&apikey=${api}`)
-        .then((res) => res.json())
-        .then((data) => {
-          dispatch(searchMoveis(data.Search));
-        });
-    };
-  }
-  
-  export function addFavoriteList(id) {
-    return {
-      type: "ADD_FAVORITE_FILM",
-      payload: {
-        id: id,
-      },
-    };
-  }
-  
-  export function removeMovieFromFavoriteList(id) {
-    return {
-      type: "REMOVE_MOVIE_FROM_FAVORITE_LIST",
-      payload: {
-        id: id,
-      },
-    };
-  }
-  
-  export function registerFavoriteList(listID) {
-    return {
-      type: "REGISTER_FAVORITES",
-      payload: {
-        listID: listID,
-      },
-    };
-  }
-  
-  export function postList(title, favoritesIDArray) {
-    return function (dispatch) {
-      let savedList = {
-        title: title,
-        movies: favoritesIDArray,
-      };
-      fetch(`https://acb-api.algoritmika.org/api/movies/list/`, {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
         },
-        body: JSON.stringify(savedList),
-      })
+    };
+}
+  
+export function fetchMovies(name) {
+    return function (dispatch) {
+        const api = "8d1d9e0f";
+        fetch(`http://www.omdbapi.com/?s=${name}&apikey=${api}`)
         .then((res) => res.json())
         .then((data) => {
-          dispatch(registerFavoriteList(data.id));
+            dispatch(searchMoveis(data.Search));
         });
     };
-  }
+}
   
-  export function getListIntoState(title, movies) {
+export function addFavoriteList(id) {
     return {
-      type: "GET_LIST_INTO_STATE",
-      payload: {
-        title: title,
-        listMovies: movies,
-      },
+        type: "ADD_FAVORITE_FILM",
+        payload: {
+        id: id,
+        },
     };
-  }
+}
   
-  export function getList(id) {
+export function removeMovieFromFavoriteList(id) {
+    return {
+        type: "REMOVE_MOVIE_FROM_FAVORITE_LIST",
+        payload: {
+        id: id,
+        },
+    };
+}
+  
+export function registerFavoriteList(listID) {
+    return {
+        type: "REGISTER_FAVORITES",
+        payload: {
+            listID: listID,
+        },
+    };
+}
+  
+export function postList(title, favoritesIDArray) {
     return function (dispatch) {
-      fetch(`https://acb-api.algoritmika.org/api/movies/list/${id}`)
+        let savedList = {
+            title: title,
+            movies: favoritesIDArray,
+        };
+        fetch(`https://acb-api.algoritmika.org/api/movies/list/`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(savedList),
+        })
         .then((res) => res.json())
         .then((data) => {
-          dispatch(getListIntoState(data.title, data.movies));
-          dispatch(getMovieInfoByImdbID(data.movies));
+            dispatch(registerFavoriteList(data.id));
         });
     };
-  }
-  export function getMovieInfoToState(movieDetails) {
+}
+  
+export function getListIntoState(title, movies) {
     return {
-      type: "GET_MOVIE_INFO_INTO_STATE",
-      payload: {
-        movieDetails: movieDetails,
-      },
+        type: "GET_LIST_INTO_STATE",
+        payload: {
+            title: title,
+            listMovies: movies,
+        },
     };
-  }
-  export function getMovieInfoByImdbID(movies) {
+}
+  
+export function getList(id) {
     return function (dispatch) {
-      let movieDetailsArray = [];
-      movies.forEach((e) => {
+        fetch(`https://acb-api.algoritmika.org/api/movies/list/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+            dispatch(getListIntoState(data.title, data.movies));
+            dispatch(getMovieInfoByImdbID(data.movies));
+        });
+    };
+}
+export function getMovieInfoToState(movieDetails) {
+    return {
+        type: "GET_MOVIE_INFO_INTO_STATE",
+        payload: {
+            movieDetails: movieDetails,
+        },
+    };
+}
+export function getMovieInfoByImdbID(movies) {
+    return function (dispatch) {
+        let movieDetailsArray = [];
+        movies.forEach((e) => {
         fetch(`http://www.omdbapi.com/?i=${e}&apikey=8d1d9e0f`)
-          .then((res) => res.json())
-          .then((data) => {
+            .then((res) => res.json())
+            .then((data) => {
             movieDetailsArray = [...movieDetailsArray, { ...data }];
             dispatch(getMovieInfoToState(movieDetailsArray));
-          });
-      });
+            });
+        });
     };
-  }
+}
   
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,30 +1,41 @@
-const intialState = {
+const initialState = {
     movies: [],
     favoriteList: [],
     title: "",
     listID: "",
     listMovies: [],
     movieDetails: [],
-};
-
-function reducer(state = intialState,action){
-    switch(action.type){
+  };
+  
+function reducer(state = initialState, action) {
+    switch (action.type) {
         case "SEARCH_MOVIE":
-            return{...state,movies:action.payload.movies,};
-        
+            return {
+                ...state,
+                movies: action.payload.movies,
+            };
+
         case "ADD_FAVORITE_FILM":
-            const newState = {...state};
+            const newState = { ...state };
             const id = action.payload.id;
+
             const match = newState.movies.find((item) => item.imdbID === id);
-            if(match){
-                newState.favoriteList = [...newState.favoriteList, {...match}];
+
+            if (match) {
+                newState.favoriteList = [...newState.favoriteList, { ...match }];
             }
             return newState;
-        
-        case "GET_MOVIE_INFO_INTO_STATE":
+
+        case "REMOVE_MOVIE_FROM_FAVORITE_LIST":
+            const newFilms = state.favoriteList.filter(
+                (item) => item.imdbID !== action.payload.id
+            );
+            return { ...state, favoriteList: newFilms };
+
+        case "REGISTER_FAVORITES":
             return {
-            ...state,
-            movieDetails: action.payload.movieDetails,
+                ...state,
+                listID: action.payload.listID,
             };
 
         case "GET_LIST_INTO_STATE":
@@ -33,6 +44,15 @@ function reducer(state = intialState,action){
                 title: action.payload.title,
                 listMovies: action.payload.listMovies,
             };
+
+        case "GET_MOVIE_INFO_INTO_STATE":
+            return {
+                ...state,
+                movieDetails: action.payload.movieDetails,
+            };
+
+        default:
+            return state;
     }
 }
 
