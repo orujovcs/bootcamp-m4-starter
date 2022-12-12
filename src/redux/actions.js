5246a5ae94d23053a7206b355da3b96bcafc0d53
orujovcs <orujovcs@gmail.com>
2022-12-12 12:16:15 +0400

    actions getmoviewithID

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -16,3 +16,46 @@ export function fetchMovies(name) {
         });
     }
 }
+
+export function getList(id) {
+    return function (dispatch) {
+        fetch(`https://acb-api.algoritmika.org/api/movies/list/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+            dispatch(getListIntoState(data.title, data.movies));
+            dispatch(getMovieInfoByImdbID(data.movies));
+        });
+    };
+}
+export function getMovieInfoToState(movieDetails) {
+    return {
+        type: "GET_MOVIE_INFO_INTO_STATE",
+        payload: {
+        movieDetails: movieDetails,
+        },
+    };
+}
+
+export function getMovieInfoByImdbID(movies) {
+    return function (dispatch) {
+        let movieDetailsArray = [];
+        movies.forEach((e) => {
+        fetch(`http://www.omdbapi.com/?i=${e}&apikey=8d1d9e0f`)
+            .then((res) => res.json())
+            .then((data) => {
+            movieDetailsArray = [...movieDetailsArray, { ...data }];
+            dispatch(getMovieInfoToState(movieDetailsArray));
+            });
+        });
+    };
+}
+export function getListIntoState(title, movies) {
+    return {
+      type: "GET_LIST_INTO_STATE",
+      payload: {
+        title: title,
+        listMovies: movies,
+      },
+    };
+  }
+  
